186ba90af50fd5de2f1963bdacbc24b3de7b7f4c
vhaf123 <victor.aranaf92@gmail.com>
2021-01-18 21:14:37 -0500

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        User::create([
+            'name' => 'Victor Arana Flores',
+            'email' => 'victor.aranaf92@gmail.com',
+            'password' => bcrypt('12345678')
+        ]);
+
+        User::factory(9)->create();
+    }
+}
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte::page')
+
+@section('title', 'Coders Free')
+
+@section('content_header')
+
+    <a class="btn btn-secondary btn-sm float-right" href="{{route('admin.posts.create')}}">Nuevo post</a>
+
+    <h1>Listado de post</h1>
+@stop
+
+@section('content')
+    @livewire('admin.posts-index')
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
